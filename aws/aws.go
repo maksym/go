bb1ae8ff8b3b84ca89d05d775a487b2220bc9be8
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2017-04-02 18:05:27 +0200

    add log args

--- aws/aws.go
+++ aws/aws.go
@@ -3,21 +3,20 @@ package main
 import "os"
 import "fmt"
 import "github.com/hpcloud/tail"
+//import "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 
 func main() {
   tailfile := os.Args[1]
+  group := os.Args[2]
+  stream := os.Args[3]
+  fmt.Println(group)
+  fmt.Println(stream)
   t, err := tail.TailFile(tailfile, tail.Config{Follow: true})
+  if err != nil {
+    fmt.Errorf("Error: %s", err)
+    return
+  }
   for line := range t.Lines {
-    fmt.Println("line: ")
     fmt.Println(line.Text)
   }
-  fmt.Println("err: ")
-  fmt.Println(err)
 }
-
-// https://docs.aws.amazon.com/sdk-for-go/api/service/cloudwatchlogs/
-// import "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
-
-// https://github.com/saymedia/journald-cloudwatch-logs
-
-// https://github.com/hpcloud/tail
