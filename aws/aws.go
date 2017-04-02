04a2198f456ef20d3bb70e3a24be466cd08ebc0d
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2017-04-02 17:25:10 +0200

    add Dockerfile

--- aws/aws.go
+++ aws/aws.go
@@ -1,23 +1,10 @@
 package main
 
 import "fmt"
-
-//func main() {
-//    fmt.Printf("hello, world\n")
-//}
-
-
-// https://docs.aws.amazon.com/sdk-for-go/api/service/cloudwatchlogs/
-// import "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
-
-// https://github.com/saymedia/journald-cloudwatch-logs
-
-// https://github.com/hpcloud/tail
-
 import "github.com/hpcloud/tail"
 
 func main() {
-  t, err := tail.TailFile("/home/max/aaa.bbb", tail.Config{Follow: true})
+  t, err := tail.TailFile("/aws/tailfile", tail.Config{Follow: true})
   for line := range t.Lines {
     fmt.Println("line: ")
     fmt.Println(line.Text)
@@ -25,3 +12,10 @@ func main() {
   fmt.Println("err: ")
   fmt.Println(err)
 }
+
+// https://docs.aws.amazon.com/sdk-for-go/api/service/cloudwatchlogs/
+// import "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+
+// https://github.com/saymedia/journald-cloudwatch-logs
+
+// https://github.com/hpcloud/tail
