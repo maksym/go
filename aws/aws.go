4b014f14bc4abea6def5cf1e4d71c56897a30156
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2017-04-02 20:30:55 +0200

    aws experiments

--- aws/aws.go
+++ aws/aws.go
@@ -3,7 +3,22 @@ package main
 import "os"
 import "fmt"
 import "github.com/hpcloud/tail"
-//import "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+import "github.com/aws/aws-sdk-go/aws"
+import "github.com/aws/aws-sdk-go/aws/session"
+import "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+
+func awsSequenceToken(group string, stream string) string {
+params := &cloudwatchlogs.DescribeLogStreamsInput{
+  LogGroupName: aws.String(group),
+  LogStreamNamePrefix: aws.String(stream),}
+  resp, err := svc.DescribeLogStreams(params)
+  if err != nil {
+    fmt.Println(err.Error())
+    return ""
+  }
+  fmt.Println(resp)
+  return ""
+}
 
 func main() {
   tailfile := os.Args[1]
@@ -11,6 +26,11 @@ func main() {
   stream := os.Args[3]
   fmt.Println(group)
   fmt.Println(stream)
+  sess := session.Must(session.NewSession())
+  svc := cloudwatchlogs.New(sess)
+
+  awsSequenceToken()
+
   t, err := tail.TailFile(tailfile, tail.Config{Follow: true})
   if err != nil {
     fmt.Errorf("Error: %s", err)
@@ -20,3 +40,12 @@ func main() {
     fmt.Println(line.Text)
   }
 }
+
+//params := &cloudwatchlogs.PutLogEventsInput{
+//  LogEvents: []*cloudwatchlogs.InputLogEvent{{
+//    Message: aws.String("EventMessage"),
+//    Timestamp: aws.Int64(1),},},
+//  LogGroupName: aws.String(group),
+//  LogStreamName: aws.String(stream),
+//  SequenceToken: aws.String("SequenceToken"),}
+// resp, err := svc.PutLogEvents(params)
